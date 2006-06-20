e2b56112775ef0fd31cc8795d58c073c0be62fea
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2006-06-20 05:30:43 +0000

    Add RecentChange object for recent changes page.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@91 f65b7e76-0091-dd46-87ae-4bc0c949c03e

---
+++ src/java/org/jmwiki/model/RecentChange.java
@@ -0,0 +1,111 @@
+/**
+ *
+ */
+package org.jmwiki.model;
+
+import java.sql.Timestamp;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ */
+public class RecentChange {
+
+	private static Logger logger = Logger.getLogger(RecentChange.class);
+	private String editComment = null;
+	private Timestamp editDate = null;
+	private int editType = -1;
+	private int topicId = -1;
+	private String topicName = null;
+	private int topicVersionId = -1;
+
+	/**
+	 *
+	 */
+	public RecentChange() {
+	}
+
+	/**
+	 *
+	 */
+	public String getEditComment() {
+		return this.editComment;
+	}
+
+	/**
+	 *
+	 */
+	public void setEditComment(String editComment) {
+		this.editComment = editComment;
+	}
+
+	/**
+	 *
+	 */
+	public Timestamp getEditDate() {
+		return this.editDate;
+	}
+
+	/**
+	 *
+	 */
+	public void setEditDate(Timestamp editDate) {
+		this.editDate = editDate;
+	}
+
+	/**
+	 *
+	 */
+	public int getEditType() {
+		return this.editType;
+	}
+
+	/**
+	 *
+	 */
+	public void setEditType(int editType) {
+		this.editType = editType;
+	}
+
+	/**
+	 *
+	 */
+	public int getTopicId() {
+		return this.topicId;
+	}
+
+	/**
+	 *
+	 */
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+
+	/**
+	 *
+	 */
+	public String getTopicName() {
+		return this.topicName;
+	}
+
+	/**
+	 *
+	 */
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+
+	/**
+	 *
+	 */
+	public int getTopicVersionId() {
+		return this.topicVersionId;
+	}
+
+	/**
+	 *
+	 */
+	public void setTopicVersionId(int topicVersionId) {
+		this.topicVersionId = topicVersionId;
+	}
+}
