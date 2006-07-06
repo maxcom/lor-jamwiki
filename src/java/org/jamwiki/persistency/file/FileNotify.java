7a6b5d3ad0fc199ba2993452bd677c62e2c743f5
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2006-07-06 20:04:17 +0000

    License:   * Add LGPL notice to all JSP files.   * Update LGPL license file name in Java files.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@262 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- src/java/org/jamwiki/persistency/file/FileNotify.java
+++ src/java/org/jamwiki/persistency/file/FileNotify.java
@@ -11,7 +11,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program (gpl.txt); if not, write to the Free Software
+ * along with this program (LICENSE.txt); if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.jamwiki.persistency.file;
