a3b4f77641f93044a92a36c235f8968b43f5c627
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2006-07-31 07:22:08 +0000

    Bump version to 0.1.2.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@538 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- src/java/org/jamwiki/WikiBase.java
+++ src/java/org/jamwiki/WikiBase.java
@@ -47,7 +47,7 @@ import org.springframework.util.StringUtils;
 public class WikiBase {
 
 	// FIXME - remove this
-	public final static String WIKI_VERSION = "0.1.1";
+	public final static String WIKI_VERSION = "0.1.2";
 	/** An instance to myself. Singleton pattern. */
 	private static WikiBase instance;
 	/** The topics are stored in a flat file */
