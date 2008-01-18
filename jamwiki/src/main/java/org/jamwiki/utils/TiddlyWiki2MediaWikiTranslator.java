d9cd39c5f29855e3c470dec38d4cf859d1793a63
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-01-18 04:28:40 +0000

    Cleanup:   * Reformat file to follow project coding standards.   * Remove unused main() method.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@1992 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki/src/main/java/org/jamwiki/utils/TiddlyWiki2MediaWikiTranslator.java
+++ jamwiki/src/main/java/org/jamwiki/utils/TiddlyWiki2MediaWikiTranslator.java
@@ -27,157 +27,147 @@ import java.util.StringTokenizer;
  * @author Michael Greifeneder mikegr@gmx.net
  */
 public class TiddlyWiki2MediaWikiTranslator {
-        private static final WikiLogger logger = WikiLogger.getLogger(TiddlyWiki2MediaWikiTranslator.class.getName());
-        
-            public static String newline = System.getProperty("line.separator");
-            
-            public String translate(String wikicode) throws IOException {
-                    String replaced = wikicode.replaceAll("\\\\n", "\n");
-                    replaced = insertBreaks(replaced);
-                    logger.info("Content with breaks?:  " + replaced);
-                    
-                    replaced = tables(headers(wikiLinks(replaced)));
-                    return replaced;
-            }
-            
-            private String tables(String wikicode) throws IOException {
-                    BufferedReader reader = new BufferedReader(new StringReader(wikicode));
-                    StringBuffer output = new StringBuffer();
-                    boolean inTable = false;
-                    String line = reader.readLine();
-                    while ( line != null) {
-                            if (inTable) { 
-                                    if (line.startsWith("|")) {
-                                           output.append("|-"); //new row
-                                           output.append(newline);
-                                           output.append(translateTableLine(line));
-                                    }
-                                    else {
-                                            output.append("|}");
-                                            output.append(newline);
-                                            output.append(line);
-                                            inTable = false;
-                                    }
-                                    
-                            }
-                            else {
-                                    if (line.startsWith("|")) { 
-                                            output.append("{|");
-                                            output.append(newline);
-                                            inTable = true;
-                                            output.append(translateTableLine(line));
-                                    }
-                                    else {
-                                            output.append(line);
-                                    }
-                            }
-                            line = reader.readLine();
-                            if (line != null) {
-                                    output.append(newline);        
-                            }
-                    }
-                    if (inTable) {
-                            output.append(newline);
-                            output.append("|}");
-                    }
-                    return output.toString();
-            }
-            
-            private String translateTableLine(String line) {
-                String[] tokens = line.split("\\|");
-                StringBuffer output = new StringBuffer();
-                output.append("|");
-                for (int i = 0; i < tokens.length; i++) {
-                    String token = tokens[i];
-                    if (i > 0) {
-                        output.append("||");
-                    }
-                    if (token.startsWith("!")) { //headers bold
-                        output.append("'''");
-                    }
-                    output.append(token);
-                    if (token.startsWith("!")) {
-                        output.append("'''");
-                    }
-                }
-                return output.toString();
-            }
-            
-            private String headers(String wikicode) throws IOException {
-                    BufferedReader reader = new BufferedReader(new StringReader(wikicode));
-                    String line = null;
-                    StringBuffer output = new StringBuffer();
-                    while ((line = reader.readLine()) != null) {
-                           int i = 0;
-                           while (line.length()>i && line.charAt(i) == '!') {
-                                   output.append('=');
-                                   i++;
-                           }
-                           output.append(line.substring(i));
-                           for (int j = 0; j < i; j++) {
-                                   output.append('=');
-                           }
-                           output.append(newline);
-                    }
-                    return output.toString();
-            }
-        
-            public String wikiLinks(String wikicode) {
-                    StringBuffer output = new StringBuffer();
-                    StringTokenizer tokenizer = new StringTokenizer(wikicode, " \t\n\r\f<>", true);
-                    while (tokenizer.hasMoreTokens()) {
-                            String token = tokenizer.nextToken();
-                            if (token.length() > 0 && Character.isUpperCase(token.charAt(0))) {
-                                    String rest = token.substring(1);
-                                    if (!rest.toLowerCase().equals(rest)) {
-                                         output.append("[[");
-                                         output.append(token);
-                                         output.append("]]");
-                                    }
-                                    else {
-                                            output.append(token);
-                                    }
-                            }
-                            else {
-                                    output.append(token);
-                            }
-                    }
-                    return output.toString();
-            }
-            
-            public String insertBreaks(String wikicode) throws IOException {
-                    BufferedReader reader = new BufferedReader(new StringReader(wikicode));
-                    
-                    StringBuffer output = new StringBuffer();
-                    boolean isLetterLast = false;
-                    boolean isLetterNow = false;
-                    String line = reader.readLine();
-                    String lastLine = null;
-                    while (line != null) {
-                            isLetterLast = isLetterNow;
-                            if (line !=null && line.length()>0 && Character.isLetterOrDigit(line.charAt(0))){
-                                    isLetterNow = true;
-                            }
-                            else {
-                                    isLetterNow = false;
-                            }
-                            if (lastLine != null) output.append(lastLine);
-                            if (isLetterLast && isLetterNow) {
-                                    output.append("<br/>");
-                            }
-                            if (lastLine != null) output.append(newline);
-                            lastLine = line;
-                            line = reader.readLine();
-                            if (line == null) {
-                                    output.append(lastLine);
-                            }
-                    }
-                    return output.toString();
-            }
- 
-            public static void main(String[] args) {
-                String t = new String("abc\\nabc");
-                String r = t.replaceAll("\\\\n", "DUMMY");
-                
-        }
+    
+	private static final WikiLogger logger = WikiLogger.getLogger(TiddlyWiki2MediaWikiTranslator.class.getName());
+
+	public static String newline = System.getProperty("line.separator");
+
+	public String translate(String wikicode) throws IOException {
+		String replaced = wikicode.replaceAll("\\\\n", "\n");
+		replaced = insertBreaks(replaced);
+		logger.info("Content with breaks?:  " + replaced);
+		replaced = tables(headers(wikiLinks(replaced)));
+		return replaced;
+	}
+
+	private String tables(String wikicode) throws IOException {
+		BufferedReader reader = new BufferedReader(new StringReader(wikicode));
+		StringBuffer output = new StringBuffer();
+		boolean inTable = false;
+		String line = reader.readLine();
+		while (line != null) {
+			if (inTable) {
+				if (line.startsWith("|")) {
+					output.append("|-"); //new row
+					output.append(newline);
+					output.append(translateTableLine(line));
+				} else {
+					output.append("|}");
+					output.append(newline);
+					output.append(line);
+					inTable = false;
+				}
+			} else {
+				if (line.startsWith("|")) {
+					output.append("{|");
+					output.append(newline);
+					inTable = true;
+					output.append(translateTableLine(line));
+				} else {
+					output.append(line);
+				}
+			}
+			line = reader.readLine();
+			if (line != null) {
+				output.append(newline);
+			}
+		}
+		if (inTable) {
+			output.append(newline);
+			output.append("|}");
+		}
+		return output.toString();
+	}
+
+	private String translateTableLine(String line) {
+		String[] tokens = line.split("\\|");
+		StringBuffer output = new StringBuffer();
+		output.append("|");
+		for (int i = 0; i < tokens.length; i++) {
+			String token = tokens[i];
+			if (i > 0) {
+				output.append("||");
+			}
+			if (token.startsWith("!")) { //headers bold
+				output.append("'''");
+			}
+			output.append(token);
+			if (token.startsWith("!")) {
+				output.append("'''");
+			}
+		}
+		return output.toString();
+	}
+
+	private String headers(String wikicode) throws IOException {
+		BufferedReader reader = new BufferedReader(new StringReader(wikicode));
+		String line = null;
+		StringBuffer output = new StringBuffer();
+		while ((line = reader.readLine()) != null) {
+			int i = 0;
+			while (line.length()>i && line.charAt(i) == '!') {
+				output.append('=');
+				i++;
+			}
+			output.append(line.substring(i));
+			for (int j = 0; j < i; j++) {
+				output.append('=');
+			}
+			output.append(newline);
+		}
+		return output.toString();
+	}
+
+	public String wikiLinks(String wikicode) {
+		StringBuffer output = new StringBuffer();
+		StringTokenizer tokenizer = new StringTokenizer(wikicode, " \t\n\r\f<>", true);
+		while (tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			if (token.length() > 0 && Character.isUpperCase(token.charAt(0))) {
+				String rest = token.substring(1);
+				if (!rest.toLowerCase().equals(rest)) {
+					output.append("[[");
+					output.append(token);
+					output.append("]]");
+				} else {
+					output.append(token);
+				}
+			} else {
+				output.append(token);
+			}
+		}
+		return output.toString();
+	}
+
+	public String insertBreaks(String wikicode) throws IOException {
+		BufferedReader reader = new BufferedReader(new StringReader(wikicode));
+		StringBuffer output = new StringBuffer();
+		boolean isLetterLast = false;
+		boolean isLetterNow = false;
+		String line = reader.readLine();
+		String lastLine = null;
+		while (line != null) {
+			isLetterLast = isLetterNow;
+			if (line !=null && line.length()>0 && Character.isLetterOrDigit(line.charAt(0))){
+				isLetterNow = true;
+			} else {
+				isLetterNow = false;
+			}
+			if (lastLine != null) {
+				output.append(lastLine);
+			}
+			if (isLetterLast && isLetterNow) {
+				output.append("<br/>");
+			}
+			if (lastLine != null) {
+				output.append(newline);
+			}
+			lastLine = line;
+			line = reader.readLine();
+			if (line == null) {
+				output.append(lastLine);
+			}
+		}
+		return output.toString();
+	}
 }
