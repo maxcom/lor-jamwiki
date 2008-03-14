c9b057168bbee60f1581485af1375bcd587273f7
dallas411 <dallas411@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-03-14 11:51:34 +0000

    Fix problem with HSQLDB in-process database locks on application reload
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2092 f65b7e76-0091-dd46-87ae-4bc0c949c03e

---
+++ jamwiki-web/src/main/java/org/jamwiki/servlets/JAMWikiListener.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, dated February 1999.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the latest version of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program (LICENSE.txt); if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.jamwiki.servlets;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.jamwiki.db.WikiDatabase;
+import org.jamwiki.utils.WikiCache;
+
+/**
+ * A ServletContextListener that will do necessary initialization
+ * & cleanup when the Servlet Context is initialized & destroyed. 
+ */
+public class JAMWikiListener implements ServletContextListener {
+
+    /* (non-Javadoc)
+     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+        WikiDatabase.initialize();
+        WikiCache.initialize();
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        WikiDatabase.shutdown();
+        WikiCache.shutdown();
+    }
+
+}
