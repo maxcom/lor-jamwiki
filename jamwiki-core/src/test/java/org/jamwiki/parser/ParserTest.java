ab49609d962d9ef7f5f6df68ed27378951f3afac
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-04-12 22:06:52 +0000

    Parser:   * Add paragraph parsing tests.  Four of the seven are currently failing.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2172 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-core/src/test/java/org/jamwiki/parser/ParserTest.java
+++ jamwiki-core/src/test/java/org/jamwiki/parser/ParserTest.java
@@ -89,6 +89,10 @@ public class ParserTest extends TestCase {
 		failures.add("HtmlMismatchTest3");
 		failures.add("HtmlMismatchTest4");
 		failures.add("UnbalancedTag1");
+		failures.add("Paragraph4");
+		failures.add("Paragraph5");
+		failures.add("Paragraph6");
+		failures.add("Paragraph7");
 		return (failures.indexOf(fileName) != -1);
 	}
 
