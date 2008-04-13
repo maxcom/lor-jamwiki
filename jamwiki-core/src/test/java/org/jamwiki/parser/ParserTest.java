cf37372dc4997b979504776fc99c8c116f4b18e8
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-04-13 17:46:58 +0000

    Parser:   * Add a (disabled) unit test for HTML lists, which are supported by     Mediawiki but not currently supported by JAMWiki.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2179 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-core/src/test/java/org/jamwiki/parser/ParserTest.java
+++ jamwiki-core/src/test/java/org/jamwiki/parser/ParserTest.java
@@ -84,6 +84,7 @@ public class ParserTest extends TestCase {
 		failures.add("HtmlXSS1");
 		failures.add("NestedTable1");
 		failures.add("NestedTable2");
+		failures.add("HtmlList1");
 		failures.add("HtmlMismatchTest3");
 		failures.add("HtmlMismatchTest4");
 		failures.add("UnbalancedTag1");
