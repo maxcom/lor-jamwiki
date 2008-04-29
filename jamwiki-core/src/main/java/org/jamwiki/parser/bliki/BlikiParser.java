8d98433cb79b803592b9e9f9f089f467e440e4ca
axelcl <axelcl@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-04-29 21:35:05 +0000

    Use new bliki 3.0 snapshot JAR
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2204 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-core/src/main/java/org/jamwiki/parser/bliki/BlikiParser.java
+++ jamwiki-core/src/main/java/org/jamwiki/parser/bliki/BlikiParser.java
@@ -1,25 +1,52 @@
 package org.jamwiki.parser.bliki;
 
-import org.jamwiki.parser.ParserOutput;
+import org.apache.commons.lang.StringUtils;
 import org.jamwiki.parser.ParserInput;
+import org.jamwiki.parser.ParserOutput;
 import org.jamwiki.parser.jflex.JFlexParser;
+import org.jamwiki.utils.Utilities;
+import org.jamwiki.utils.WikiLogger;
 
 public class BlikiParser extends JFlexParser {
+	private static final WikiLogger logger = WikiLogger.getLogger(BlikiParser.class.getName());
+
 	public BlikiParser(ParserInput parserInput) {
 		super(parserInput);
 	}
 
 	/**
-	 * Parse text for online display.
+	 * Returns a HTML representation of the given wiki raw text for online representation.
+	 *
+	 * @param parserOutput A ParserOutput object containing parser
+	 *  metadata output.
+	 * @param raw The raw Wiki syntax to be converted into HTML.
+	 * @return The parsed content.
+	 * @throws Exception Thrown if any error occurs during parsing.
 	 */
-	public String parseHTML(String raw) throws Exception {
-		ParserOutput doc = new ParserOutput();
-
-		String titlePrefix = "/jamwiki/" + parserInput.getVirtualWiki() + '/';
-		JAMWikiModel wikiModel = new JAMWikiModel(parserInput, doc, titlePrefix + "${image}", titlePrefix + "${title}");
-		String htmlStr = wikiModel.render(raw);
-		htmlStr = htmlStr == null ? "" : htmlStr;
-		return htmlStr;
+	public String parseHTML(ParserOutput parserOutput, String raw) throws Exception {
+		long start = System.currentTimeMillis();
+		String output = null;
+		if (!StringUtils.isBlank(this.isRedirect(raw))) {
+			// redirects are parsed differently
+			output = this.parseRedirect(parserOutput, raw);
+		} else {
+			String context = parserInput.getContext();
+			String baseURL = "";
+			if (context != null) {
+				baseURL += context;
+			}
+			// context never ends with a "/" per servlet specification
+			baseURL += "/";
+			// get the virtual wiki, which should have been set by the parent servlet
+			baseURL += Utilities.encodeForURL(parserInput.getVirtualWiki());
+			baseURL += "/";
+			JAMWikiModel wikiModel = new JAMWikiModel(parserInput, parserOutput, baseURL + "${image}", baseURL + "${title}");
+			output = wikiModel.render(new JAMHTMLConverter(parserInput), raw);
+			output = output == null ? "" : output;
+		}
+		String topicName = (!StringUtils.isBlank(this.parserInput.getTopicName())) ? this.parserInput.getTopicName() : null;
+		logger.info("Parse time (parseHTML) for " + topicName + " (" + ((System.currentTimeMillis() - start) / 1000.000) + " s.)");
+		return output;
 	}
 
 }
---
+++ jamwiki-core/src/main/java/org/jamwiki/parser/bliki/JAMHTMLConverter.java
@@ -0,0 +1,43 @@
+package org.jamwiki.parser.bliki;
+
+import java.io.IOException;
+
+import info.bliki.wiki.filter.HTMLConverter;
+import info.bliki.wiki.model.IWikiModel;
+import info.bliki.wiki.model.ImageFormat;
+
+import org.htmlcleaner.TagNode;
+import org.jamwiki.parser.ParserInput;
+import org.jamwiki.utils.LinkUtil;
+import org.jamwiki.utils.NamespaceHandler;
+
+public class JAMHTMLConverter extends HTMLConverter {
+	private static final int DEFAULT_THUMBNAIL_SIZE = 180;
+
+	private ParserInput fParserInput; 
+
+	public JAMHTMLConverter(ParserInput parserInput) {
+		super(); 
+		fParserInput = parserInput;
+	}
+	
+	public void imageNodeToText(TagNode imageTagNode, ImageFormat imageFormat, Appendable resultBuffer, IWikiModel model) throws IOException {
+		String imageName = imageFormat.getFilename();
+		imageName = imageName.replaceAll("_", " ");
+		int maxDimension = imageFormat.getSize();
+		String type = imageFormat.getType();
+		boolean frame = type == null ? false : type.equals("frame");
+		boolean thumb = type == null ? false : type.equals("thumb");
+		if (thumb && maxDimension <= 0) {
+			maxDimension = DEFAULT_THUMBNAIL_SIZE;
+		}
+		try {
+			resultBuffer.append(LinkUtil.buildImageLinkHtml(fParserInput.getContext(), fParserInput.getVirtualWiki(), model
+					.getImageNamespace()
+					+ NamespaceHandler.NAMESPACE_SEPARATOR + imageName, frame, thumb, imageFormat.getLocation(), imageFormat.getCaption(),
+					maxDimension, false, null, false));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
