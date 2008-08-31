e74b3ea9ac97dee8b7ee2f0f8ef66cf646f86f0d
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2008-08-31 03:52:51 +0000

    Spring Security:   * Remove custom JAMWikiLogoutFilter by making better use of existing Spring     Security functionality.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2275 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-web/src/main/java/org/jamwiki/servlets/LoginServlet.java
+++ jamwiki-web/src/main/java/org/jamwiki/servlets/LoginServlet.java
@@ -16,9 +16,17 @@
  */
 package org.jamwiki.servlets;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringUtils;
+import org.jamwiki.Environment;
+import org.jamwiki.WikiBase;
+import org.jamwiki.model.VirtualWiki;
 import org.jamwiki.utils.WikiLogger;
+import org.jamwiki.utils.WikiUtil;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -33,6 +41,43 @@ public class LoginServlet extends JAMWikiServlet {
 	 *
 	 */
 	protected ModelAndView handleJAMWikiRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView next, WikiPageInfo pageInfo) throws Exception {
+		if (ServletUtil.isTopic(request, "Special:Logout")) {
+			// redirect to the Spring Security logout
+			handleLogout(request, response);
+			return null;
+		}
 		return ServletUtil.viewLogin(request, pageInfo, null, null);
+	}
+	
+	/**
+	 * Redirect to the default Spring Security logout URL after adding a "successful logout"
+	 * URL to the request.
+	 */
+	private void handleLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String virtualWikiName = WikiUtil.getVirtualWikiFromURI(request);
+		if (StringUtils.isBlank(virtualWikiName)) {
+			virtualWikiName = WikiBase.DEFAULT_VWIKI;
+		}
+		String topicName = Environment.getValue(Environment.PROP_BASE_DEFAULT_TOPIC);
+		try {
+			VirtualWiki virtualWiki = WikiBase.getDataHandler().lookupVirtualWiki(virtualWikiName);
+			topicName = virtualWiki.getDefaultTopicName();
+		} catch (Exception e) {
+			logger.warning("Unable to retrieve default topic for virtual wiki", e);
+		}
+		String logoutSuccessUrl = "/" + virtualWikiName + "/" + topicName;
+		try {
+			logoutSuccessUrl = URLEncoder.encode(logoutSuccessUrl, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// this should never happen
+			throw new IllegalStateException("Unsupporting encoding UTF-8");
+		}
+		String springSecurityLogoutUrl = request.getContextPath();
+		if (StringUtils.equals(request.getContextPath(), "/")) {
+			springSecurityLogoutUrl = "";
+		}
+		// FIXME - do not hard code spring logout URL
+		springSecurityLogoutUrl += "/j_spring_security_logout?filterProcessesUrl=" + logoutSuccessUrl;
+		response.sendRedirect(springSecurityLogoutUrl);
 	}
 }
