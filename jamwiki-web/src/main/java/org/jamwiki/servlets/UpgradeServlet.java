b9a9b3f4b51c9612b7dba733d9bb91ff037e5b17
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2009-01-12 05:30:32 +0000

    Look & Feel:   * Make a slight change to the CSS so that textarea boxes on the     Special:Translation page allow overflow.   * Add code so that the StyleSheet topic automatically updates during upgrades.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2420 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-web/src/main/java/org/jamwiki/servlets/UpgradeServlet.java
+++ jamwiki-web/src/main/java/org/jamwiki/servlets/UpgradeServlet.java
@@ -153,6 +153,9 @@ public class UpgradeServlet extends JAMWikiServlet {
 		if (oldVersion.before(0, 6, 7)) {
 			stylesheet = true;
 		}
+		if (oldVersion.before(0, 7, 0)) {
+			stylesheet = true;
+		}
 		if (stylesheet) {
 			// upgrade stylesheet
 			if (!upgradeStyleSheet(request, messages)) {
