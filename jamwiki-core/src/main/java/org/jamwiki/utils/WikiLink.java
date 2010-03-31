a88bae1bfcb13b92441f2542acf78315021e0dc7
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2010-03-31 05:12:01 +0000

    Namespaces:   * If a namespace is not case-sensitive (user, template, category) then the     associated comments namespace should not be case-sensitive either.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/trunk@2992 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-core/src/main/java/org/jamwiki/utils/WikiLink.java
+++ jamwiki-core/src/main/java/org/jamwiki/utils/WikiLink.java
@@ -63,8 +63,20 @@ public class WikiLink {
 	 * others (such as the user namespace) are not.
 	 */
 	public boolean isCaseSensitive() {
-		// user/template/category namespaces are case-insensitive
-		return (!this.namespace.getId().equals(Namespace.SPECIAL_ID) && !this.namespace.getId().equals(Namespace.TEMPLATE_ID) && !this.namespace.getId().equals(Namespace.USER_ID) && !this.namespace.getId().equals(Namespace.CATEGORY_ID));
+		// user/template/category namespaces are not case-insensitive
+		if (this.namespace.getId().equals(Namespace.SPECIAL_ID)) {
+			return false;
+		}
+		if (this.namespace.getId().equals(Namespace.TEMPLATE_ID) || this.namespace.getId().equals(Namespace.TEMPLATE_COMMENTS_ID)) {
+			return false;
+		}
+		if (this.namespace.getId().equals(Namespace.USER_ID) || this.namespace.getId().equals(Namespace.USER_COMMENTS_ID)) {
+			return false;
+		}
+		if (this.namespace.getId().equals(Namespace.CATEGORY_ID) || this.namespace.getId().equals(Namespace.CATEGORY_COMMENTS_ID)) {
+			return false;
+		}
+		return true;
 	}
 
 	/**
