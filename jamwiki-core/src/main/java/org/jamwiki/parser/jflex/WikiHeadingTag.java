94dca8f5f9c9c75e37fe807a51e7102b08d0b9c0
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2010-12-11 01:20:00 +0000

    Parser:   * Headings of seven or more levels caused generation of invalid HTML.     Reported by an anonymous user.
    git-svn-id: https://jamwiki.svn.sourceforge.net/svnroot/jamwiki/wiki/branches/0.9.x@3307 f65b7e76-0091-dd46-87ae-4bc0c949c03e

--- jamwiki-core/src/main/java/org/jamwiki/parser/jflex/WikiHeadingTag.java
+++ jamwiki-core/src/main/java/org/jamwiki/parser/jflex/WikiHeadingTag.java
@@ -16,6 +16,8 @@
  */
 package org.jamwiki.parser.jflex;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jamwiki.DataAccessException;
 import org.jamwiki.parser.ParserException;
 import org.jamwiki.parser.ParserInput;
@@ -23,7 +25,6 @@ import org.jamwiki.parser.TableOfContents;
 import org.jamwiki.utils.LinkUtil;
 import org.jamwiki.utils.Utilities;
 import org.jamwiki.utils.WikiLogger;
-import org.apache.commons.lang.StringEscapeUtils;
 
 /**
  * This class parses wiki headings of the form <code>==heading content==</code>.
@@ -81,7 +82,17 @@ public class WikiHeadingTag implements JFlexParserTag {
 		int level = (Integer)args[0];
 		String tagText = raw.substring(level, raw.length() - level).trim();
 		ParserInput tmpParserInput = new ParserInput(lexer.getParserInput());
-		String tocText = JFlexParserUtil.parseFragment(tmpParserInput, tagText, JFlexParser.MODE_PROCESS);
+		// special case - if text is of the form "=======text=======" then after stripping equals
+		// signs "=text=" will be left.  in this one case strip any opening equals signs before parsing.
+		String extraEqualSigns = "";
+		int pos = StringUtils.indexOfAnyBut(tagText, "= \t");
+		if (pos != -1) {
+			extraEqualSigns = tagText.substring(0, pos);
+			tagText = (pos < tagText.length()) ? tagText.substring(pos) : "";
+		}
+		String tocText = JFlexParserUtil.parseFragment(tmpParserInput, tagText, lexer.getMode());
+		tocText = extraEqualSigns + tocText;
+		String parsedTocText = tocText;
 		tocText = Utilities.stripMarkup(tocText);
 		String tagName = lexer.getParserInput().getTableOfContents().checkForUniqueName(tocText);
 		// re-convert any &uuml; or other (converted by the parser) entities back
@@ -95,7 +106,7 @@ public class WikiHeadingTag implements JFlexParserTag {
 		output.append("<a name=\"").append(Utilities.encodeAndEscapeTopicName(tagName)).append("\"></a>");
 		output.append("<h").append(level).append('>');
 		output.append(this.buildSectionEditLink(lexer.getParserInput(), nextSection));
-		output.append("<span>").append(JFlexParserUtil.parseFragment(lexer.getParserInput(), tagText, lexer.getMode())).append("</span>");
+		output.append("<span>").append(parsedTocText).append("</span>");
 		output.append("</h").append(level).append('>');
 		return output.toString();
 	}
