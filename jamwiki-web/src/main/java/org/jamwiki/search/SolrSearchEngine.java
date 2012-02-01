b622efeeb48bb3c06f1be6be7a3a961c579daa3a
Ildar Hizbulin <hizel@vyborg.ru>
2012-02-01 13:43:13 +0400

    мы индексируем в solr и ломаем поиск основного сайта :-]

--- jamwiki-web/src/main/java/org/jamwiki/search/SolrSearchEngine.java
+++ jamwiki-web/src/main/java/org/jamwiki/search/SolrSearchEngine.java
@@ -16,12 +16,19 @@
  */
 package org.jamwiki.search;
 
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+import org.jamwiki.Environment;
 import org.jamwiki.SearchEngine;
-import org.jamwiki.model.SearchResultEntry;
-import org.jamwiki.model.Topic;
+import org.jamwiki.WikiBase;
+import org.jamwiki.model.*;
+import org.jamwiki.utils.Pagination;
 import org.jamwiki.utils.WikiLogger;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -32,16 +39,71 @@ import java.util.List;
 public class SolrSearchEngine implements SearchEngine {
   private static final WikiLogger logger = WikiLogger.getLogger(LuceneSearchEngine.class.getName());
   
+  private SolrServer solrServer;
+  private boolean autoCommit = true;
+
+  public SolrSearchEngine() throws Exception {
+    solrServer = new CommonsHttpSolrServer(Environment.getValue(Environment.PROP_BASE_SEARCH_SOLR_URL));
+  }
+
+  private void commit(boolean commitNow) throws SolrServerException, IOException {
+    if(commitNow) {
+      solrServer.commit();
+    }
+  }
+  
+  private String genId(Topic topic) {
+    return String.format("WIKI%d", topic.getTopicId());
+  }
+  
   public void addToIndex(Topic topic) {
-    
+    if(topic.getTopicType() == TopicType.REDIRECT) {
+      return;
+    }
+    try {
+      SolrInputDocument doc = new SolrInputDocument();
+      doc.addField("id", genId(topic));
+      doc.addField("section_id", 0);
+
+      List<RecentChange> changes = WikiBase.getDataHandler().getTopicHistory(topic, new Pagination(1, 0), true);
+      if(changes.isEmpty()) {
+        doc.addField("user_id", 0);
+        doc.addField("postdate", new Timestamp(0));
+      } else {
+        RecentChange lastChange = changes.get(0);
+        doc.addField("user_id", lastChange.getAuthorId());
+        doc.addField("postdate", lastChange.getChangeDate());
+      }
+      doc.addField("topic_user_id", 0);
+      doc.addField("topic_id", topic.getTopicId());
+      doc.addField("group_id", 0);
+      doc.addField("title", topic.getName());
+      doc.addField("topic_title", topic.getName());
+      doc.addField("message", topic.getTopicContent());
+      ;
+      doc.addField("is_comment", false);
+      solrServer.add(doc);
+      commit(autoCommit);
+    } catch (Exception e) {
+      logger.error("Exception while adding topic " + topic.getVirtualWiki() + " / " + topic.getName(), e);
+    }
   }
   
   public void commit(String virtualWiki) {
-    
+    try {
+      commit(true);
+    } catch (Exception e) {
+      logger.error("Exception while committing pending changes for virtual wiki " + virtualWiki, e);
+    }
   }
   
   public void deleteFromIndex(Topic topic) {
-
+    try {
+      solrServer.deleteById(genId(topic));
+      commit(autoCommit);
+    } catch (Exception e) {
+      logger.error("Exception while remove topic " + topic.getName(), e);
+    }
   }
 
   public List<SearchResultEntry> findResults(String virtualWiki, String text, List<Integer> namespaces) {
@@ -49,18 +111,32 @@ public class SolrSearchEngine implements SearchEngine {
   }
 
   public void refreshIndex() throws Exception {
-
+    List<VirtualWiki> allWikis = WikiBase.getDataHandler().getVirtualWikiList();
+    Topic topic;
+    for (VirtualWiki virtualWiki : allWikis) {
+      List<String> topicNames = WikiBase.getDataHandler().getAllTopicNames(virtualWiki.getName(), false);
+      for (String topicName : topicNames) {
+        topic = WikiBase.getDataHandler().lookupTopic(virtualWiki.getName(), topicName, false);
+        addToIndex(topic);
+      }
+    }
   }
 
   public void setAutoCommit(boolean autoCommit) {
-
+    this.autoCommit = autoCommit;
   }
 
   public void shutdown() throws IOException {
-
+    logger.debug("Okay");
   }
 
   public void updateInIndex(Topic topic) {
-
+    try {
+      deleteFromIndex(topic);
+      addToIndex(topic);
+      commit(autoCommit);
+    } catch (Exception e) {
+      logger.error("Exception while updating topic " + topic.getVirtualWiki() + " / " + topic.getName(), e);
+    }
   }
 }
